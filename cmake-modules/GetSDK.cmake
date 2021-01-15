77a4a1a7ac270fa763fca5b41036c10f6d35948d
Victor Vazquez <vhvb1989@gmail.com>
2021-01-15 01:14:02 +0000

    build all

--- cmake-modules/GetSDK.cmake
+++ cmake-modules/GetSDK.cmake
@@ -2,7 +2,7 @@
 include(FetchContent)
 FetchContent_Declare(cppstoragesdk
     GIT_REPOSITORY      https://github.com/Azure/azure-sdk-for-cpp.git
-    GIT_TAG             azure-template_1.0.0-beta.3)
+    GIT_TAG             azure-storage-files-datalake_12.0.0-beta.6)
 FetchContent_GetProperties(cppstoragesdk)
 if(NOT cppstoragesdk_POPULATED)
     FetchContent_Populate(cppstoragesdk)
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
-#include "azure/storage/blobs/blob.hpp"
+#include "azure/storage/blobs.hpp"
 
 #include "helpers.hpp"
 
 using namespace Azure::Storage::Blobs;
-namespace fs = std::filesystem;
 
 int main(int argc, char* argv[])
 {
-  auto currentPath = fs::current_path();
-  auto mainContainerName = currentPath.filename().string();
 
   /**************** Container sdk client ************************/
   /****************   Create container  ************************/
   auto containerClient = BlobContainerClient::CreateFromConnectionString(
-      std::getenv("STORAGE_CONNECTION_STRING"), mainContainerName);
+      std::getenv("STORAGE_CONNECTION_STRING"), "aaa");
   try
   {
     containerClient.Create();
@@ -26,53 +23,6 @@ int main(int argc, char* argv[])
   /**************** Container sdk client ************************/
   /****************      list Blobs     ************************/
   // 5000 max blobs supported by call. It's fine for this app
-  auto blobListResponse = containerClient.ListBlobsFlatSegment();
-  auto blobList = blobListResponse.ExtractValue();
-
-  for (auto& item : fs::directory_iterator(currentPath))
-  {
-    auto fileName = item.path().filename().string();
-    if (shouldSkip(item, fileName))
-    {
-      continue;
-    }
-
-    /****************    Block Blob Client            **************/
-    /****************  get blob from container client *************/
-    auto blobClient = containerClient.GetBlockBlobClient(fileName);
-
-    // check if file is already in storage
-    auto blob = findBlob(blobList.Items, [fileName](std::vector<BlobItem>::iterator i) {
-      return i->Name == fileName;
-    });
-
-    if (blob == blobList.Items.end())
-    { // new File. Upload
-      std::cout << std::endl << "New file: " << fileName << ". Uploading.";
-
-      /****************    Block Blob Client            **************/
-      /****************        Upload                   *************/
-      blobClient.UploadFrom(item.path().string());
-
-      // move on to next file
-      continue;
-    }
-
-    // Remove blob from the blob list
-    blobList.Items.erase(blob);
-  }
-
-  // All blobs still in bloblist need to be download
-  for (auto blob : blobList.Items)
-  {
-    auto blobName = blob.Name;
-    std::cout << std::endl << "Downloading file: " << blobName;
-
-    /****************    Block Blob Client            **************/
-    /****************        Upload                   *************/
-    BlockBlobClient blobClient = containerClient.GetBlockBlobClient(blobName);
-    blobClient.DownloadTo(blobName);
-  }
 
   std::cout << std::endl << std::endl << "Sync completed " << std::endl;
   return 0;
