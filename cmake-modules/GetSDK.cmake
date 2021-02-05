6766a218969a06d38689478d8f263ef04833a646
Victor Vazquez <vhvb1989@gmail.com>
2021-02-05 18:11:35 +0000

    update latest release

--- cmake-modules/GetSDK.cmake
+++ cmake-modules/GetSDK.cmake
@@ -2,7 +2,7 @@
 include(FetchContent)
 FetchContent_Declare(cppstoragesdk
     GIT_REPOSITORY      https://github.com/Azure/azure-sdk-for-cpp.git
-    GIT_TAG             azure-storage-files-datalake_12.0.0-beta.6)
+    GIT_TAG             azure-storage-files-datalake_12.0.0-beta.7)
 FetchContent_GetProperties(cppstoragesdk)
 if(NOT cppstoragesdk_POPULATED)
     FetchContent_Populate(cppstoragesdk)
